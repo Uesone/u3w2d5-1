b32aa2082f98f3aa39c8a4c732ef47d1ca3cd9c9
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-08-02 12:43:01 +0200

    Update Libro.java

--- src/main/java/com/example/catalogo/Libro.java
+++ src/main/java/com/example/catalogo/Libro.java
@@ -1,4 +1,28 @@
 package com.example.catalogo;
 
-public class Libro {
+public class Libro extends ElementoCatalogo {
+    private String autore;
+    private String genere;
+
+    public Libro(String codiceISBN, String titolo, int annoPubblicazione, int numeroPagine, String autore, String genere) {
+        super(codiceISBN, titolo, annoPubblicazione, numeroPagine);
+        this.autore = autore;
+        this.genere = genere;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "autore='" + autore + '\'' +
+                ", genere='" + genere + '\'' +
+                "} " + super.toString();
+    }
 }
