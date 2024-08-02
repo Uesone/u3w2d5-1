87cf33747b19608db22126245916d0432826b037
Umberto Amoroso <34135219+Uesone@users.noreply.github.com>
2024-08-02 12:03:22 +0200

    Initial commit

---
+++ src/main/java/com/example/catalogobibliotecario/CatalogoBibliotecario.java
@@ -0,0 +1,8 @@
+package com.example.catalogobibliotecario;
+
+public class CatalogoBibliotecario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/example/catalogobibliotecario/Libro.java
@@ -0,0 +1,4 @@
+package com.example.catalogobibliotecario;
+
+public class Libro {
+}
